f54b5aaa402d8876a698a06c0f322264a2da34b4
Benjamin Townsend <benjamin@bentownsend.com>
2023-12-11 23:58:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
+$(document).ready(function () {
     let currentNewsIndex = 0;
-    const newsItems = document.querySelectorAll('.news-item');
+    const newsItems = $('.news-item');
     const totalNewsItems = newsItems.length;
 
-    setInterval(() => {
-        newsItems[currentNewsIndex].style.display = 'none';
-        currentNewsIndex = (currentNewsIndex + 1) % totalNewsItems;
-        newsItems[currentNewsIndex].style.display = 'inline-flex';
-    }, 15000); // Change news item every 5 seconds
+    // Initially hide all news items except the first one
+    newsItems.hide().eq(0).show();
+
+    setInterval(function() {
+        newsItems.eq(currentNewsIndex).fadeOut(function() {
+            currentNewsIndex = (currentNewsIndex + 1) % totalNewsItems;
+            newsItems.eq(currentNewsIndex).fadeIn();
+        });
+    }, 5000); // Change news item every 5 seconds
 });
